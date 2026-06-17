2ecc15d81086f646a6bef7f43d86f9e7f8d03fd6
Dhanush <dhanushkandhan75@gmail.com>
2026-06-18 01:17:56 +0530

    chore: updates in profile info

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -3,7 +3,7 @@ export const AppConfig = {
   title: 'Dhanush Kandhan',
   headline: 'Engineer & Explorer',
   description:
-    'A Curious Engineer and Explorer who loves to build scalable AI systems and modern software that solves real problems.',
+    'A Curious Engineer and Explorer who loves to build scalable AI systems and modern software applications that solves real problems.',
   locale: 'en',
   url: 'https://dhanu.letretro.com',
   author: 'Dhanush Kandhan',
